9a5ddefff4a3c19129995ad6db36b6b11a6493d9
Anthony Smigielski <anthony.smigielski@protonmail.com>
2026-02-10 09:09:41 +0800

    style(card): set bg opacity to 40% (bg-card/40) and replace empty interface with type alias

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+export type CardProps = React.HTMLAttributes<HTMLDivElement>
 
 const Card = React.forwardRef<HTMLDivElement, CardProps>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow p-6",
+      "rounded-xl border bg-card/60 text-card-foreground shadow p-6",
       className
     )}
     {...props}
